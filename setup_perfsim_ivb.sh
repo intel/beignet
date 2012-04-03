50b10a90b8cea51e2219d9ea88deed4a08249eaf
Benjamin Segovia <segovia.benjamin@gmail.com>
2012-04-03 20:32:03 +0000

    Added a script to support the performance simulator

---
+++ setup_perfsim_ivb.sh
@@ -0,0 +1,4 @@
+export INTEL_DEVID_OVERRIDE=0x0166     # or, 0x0112
+export DEVICE=ivb_m_gt2                #     snb_gt2 for SNB GT2 desktop
+export OCL_SIMULATOR=2                 # 0 -> HW, 1 -> fulsim, 2 -> perfsim
+
