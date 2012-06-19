beeb5f3a3fcf81a2d26ad7db5879f5695b084aa0
Benjamin Segovia <devnull@localhost>
2012-06-19 23:59:18 +0000

    Removed unused file

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-./test_2d_copy ../../kernels/test_2d_copy/test_2d_copy_kernels_0.bin
-./test_barrier ../../kernels/test_barrier/test_barrier_kernels_0.bin
-./test_constant_memory ../../kernels/test_constant_memory/test_constant_memory_0.bin
-./test_copy_buffer ../../kernels/copybuffer/CopyBuffer_0.bin
-./test_imm_parameters ../../kernels/test_imm_parameters/test_imm_parameters_kernels_0.bin
-./test_local_memory ../../kernels/test_local_memory/test_local_memory_0.bin
-./test_memory_leak ../../kernels/copybuffer/CopyBuffer_0.bin
-./test_static_local_memory ../../kernels/test_static_local_memory/test_static_local_memory_0.bin
-
