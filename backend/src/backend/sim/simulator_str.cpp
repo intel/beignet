1d8eaa446ae23f27421ece63af0fa1c990c035fa
bsegovia <segovia.benjamin@gmail.com>
2012-08-13 16:17:43 +0000

    Reenabled the CMake build path. It is really imperfect and the build CMake scripts should be cleanly unified later

--- backend/src/backend/sim/simulator_str.cpp
+++ backend/src/backend/sim/simulator_str.cpp
@@ -1,46 +1,23 @@
-/* 
- * Copyright © 2012 Intel Corporation
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Benjamin Segovia <benjamin.segovia@intel.com>
- */
-
 #include "string"
 namespace gbe {
 std::string simulator_str = 
-"/*\n"
-" * Copyright 2012 Intel Corporation\n"
+"/* \n"
+" * Copyright © 2012 Intel Corporation\n"
 " *\n"
-" * Permission is hereby granted, free of charge, to any person obtaining a\n"
-" * copy of this software and associated documentation files (the \"Software\"),\n"
-" * to deal in the Software without restriction, including without limitation\n"
-" * the rights to use, copy, modify, merge, publish, distribute, sublicense,\n"
-" * and/or sell copies of the Software, and to permit persons to whom the\n"
-" * Software is furnished to do so, subject to the following conditions:\n"
+" * This library is free software; you can redistribute it and/or\n"
+" * modify it under the terms of the GNU Lesser General Public\n"
+" * License as published by the Free Software Foundation; either\n"
+" * version 2 of the License, or (at your option) any later version.\n"
 " *\n"
-" * The above copyright notice and this permission notice (including the next\n"
-" * paragraph) shall be included in all copies or substantial portions of the\n"
-" * Software.\n"
+" * This library is distributed in the hope that it will be useful,\n"
+" * but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+" * Lesser General Public License for more details.\n"
 " *\n"
-" * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
-" * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
-" * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL\n"
-" * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
-" * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
-" * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER\n"
-" * DEALINGS IN THE SOFTWARE.\n"
+" * You should have received a copy of the GNU Lesser General Public\n"
+" * License along with this library. If not, see <http://www.gnu.org/licenses/>.\n"
+" *\n"
+" * Author: Benjamin Segovia <benjamin.segovia@intel.com>\n"
 " */\n"
 "\n"
 "/**\n"
--- backend/src/ocl_stdlib_str.cpp
+++ backend/src/ocl_stdlib_str.cpp
@@ -1,25 +1,25 @@
-/* 
- * Copyright © 2012 Intel Corporation
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- *
- * Author: Benjamin Segovia <benjamin.segovia@intel.com>
- */
-
 #include "string"
 namespace gbe {
 std::string ocl_stdlib_str = 
+"/* \n"
+" * Copyright © 2012 Intel Corporation\n"
+" *\n"
+" * This library is free software; you can redistribute it and/or\n"
+" * modify it under the terms of the GNU Lesser General Public\n"
+" * License as published by the Free Software Foundation; either\n"
+" * version 2 of the License, or (at your option) any later version.\n"
+" *\n"
+" * This library is distributed in the hope that it will be useful,\n"
+" * but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+" * Lesser General Public License for more details.\n"
+" *\n"
+" * You should have received a copy of the GNU Lesser General Public\n"
+" * License along with this library. If not, see <http://www.gnu.org/licenses/>.\n"
+" *\n"
+" * Author: Benjamin Segovia <benjamin.segovia@intel.com>\n"
+" */\n"
+"\n"
 "/////////////////////////////////////////////////////////////////////////////\n"
 "// OpenCL basic types\n"
 "/////////////////////////////////////////////////////////////////////////////\n"
