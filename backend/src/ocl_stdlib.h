a3bea41996823ac251d647865825ff8035729ea1
Homer Hsing <homer.xing@intel.com>
2012-12-06 09:33:55 +0800

    support OpenCL 1.1 floating-point macros

--- backend/src/ocl_stdlib.h
+++ backend/src/ocl_stdlib.h
@@ -61,6 +61,37 @@ DEF(float);
 #define __CL_VERSION_1_0__ 100
 #define __CL_VERSION_1_1__ 110
 /////////////////////////////////////////////////////////////////////////////
+// OpenCL floating-point macros and pragmas
+/////////////////////////////////////////////////////////////////////////////
+#define FLT_DIG 6
+#define FLT_MANT_DIG 24
+#define FLT_MAX_10_EXP +38
+#define FLT_MAX_EXP +128
+#define FLT_MIN_10_EXP -37
+#define FLT_MIN_EXP -125
+#define FLT_RADIX 2
+#define FLT_MAX 0x1.fffffep127f
+#define FLT_MIN 0x1.0p-126f
+#define FLT_EPSILON 0x1.0p-23f
+
+#define MAXFLOAT     3.40282347e38F
+#define HUGE_VALF    (__builtin_huge_valf())
+#define INFINITY     (__builtin_inff())
+#define NAN          (__builtin_nanf(\"\"))
+#define M_E_F        2.718281828459045F
+#define M_LOG2E_F    1.4426950408889634F
+#define M_LOG10E_F   0.43429448190325176F
+#define M_LN2_F      0.6931471805599453F
+#define M_LN10_F     2.302585092994046F
+#define M_PI_F       3.141592653589793F
+#define M_PI_2_F     1.5707963267948966F
+#define M_PI_4_F     0.7853981633974483F
+#define M_1_PI_F     0.3183098861837907F
+#define M_2_PI_F     0.6366197723675814F
+#define M_2_SQRTPI_F 1.1283791670955126F
+#define M_SQRT2_F    1.4142135623730951F
+#define M_SQRT1_2_F  0.7071067811865476F
+/////////////////////////////////////////////////////////////////////////////
 // OpenCL address space
 /////////////////////////////////////////////////////////////////////////////
 #define __private __attribute__((address_space(0)))
