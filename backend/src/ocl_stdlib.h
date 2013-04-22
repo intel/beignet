7459d19f30dea14c43a70bf3b09b3c605c99699a
Simon Richter <Simon.Richter@hogyros.de>
2013-04-22 18:29:56 +0200

    Fix typo
    Signed-off-by: Simon Richter <Simon.Richter@hogyros.de>

--- backend/src/ocl_stdlib.h
+++ backend/src/ocl_stdlib.h
@@ -1,5 +1,5 @@
 /* 
-uint* Copyright © 2012 Intel Corporation
+ * Copyright © 2012 Intel Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
