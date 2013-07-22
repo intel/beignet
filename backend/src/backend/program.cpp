75b59f27c0bf403e97925e32b407b4fcb9bcb73f
Yang Rong <rong.r.yang@intel.com>
2013-07-22 13:24:18 +0800

    Add build clang option fno-builtin to disable intrinsics.
    Signed-off-by: Yang Rong <rong.r.yang@intel.com>
    Reviewed-by: Zhigang Gong <zhigang.gong@linux.intel.com>

--- backend/src/backend/program.cpp
+++ backend/src/backend/program.cpp
@@ -150,6 +150,9 @@ namespace gbe {
     }
 
     args.push_back("-emit-llvm");
+    // XXX we haven't implement those builtin functions,
+    // so disable it currently.
+    args.push_back("-fno-builtin");
     if(bOpt)  args.push_back("-O3");
 #if LLVM_VERSION_MINOR <= 2
     args.push_back("-triple");
