cecd52124bfd7c79108e36cae549354b16c2ef4e
Zhenyu Wang <zhenyuw@linux.intel.com>
2014-04-10 18:09:44 +0800

    Use pkg-config to check modules
    Instead of use pre-defined path for dependent modules, e.g libdrm,
    libdrm_intel, etc. Use pkg-config helper for cmake instead. This makes
    it easy to work with developer own built version of those dependences.
    
    Also remove libGL dependence for 'gbe_bin_generator' which is not required.
    libutest.so still requires libGL now but might be fixed by checking real
    GL dependence.
    
    v2: Fix build with mesa source (92e6260) and link required EGL lib with utests too.
    
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Reviewed-by：Zhigang Gong <zhigang.gong@linux.intel.com>

--- CMake/FindMesaSrc.cmake
+++ CMake/FindMesaSrc.cmake
@@ -1,37 +1,10 @@
 #
-# Try to find EGL library and include path.
+# Try to find mesa source code
 # Once done this will define
 #
-# EGL_FOUND
-# EGL_INCLUDE_PATH
-# EGL_LIBRARY
+# MESA_SOURCE_FOUND
+# MESA_SOURCE_INCLUDES
 #
-
-FIND_PATH(EGL_INCLUDE_PATH EGL/egl.h
-  ~/include/
-  /usr/include/
-  /usr/local/include/
-  /sw/include/
-  /opt/local/include/
-  DOC "The directory where gen/program.h resides")
-FIND_LIBRARY(EGL_LIBRARY
-  NAMES EGL egl
-  PATHS
-  ~/lib/
-  /usr/lib64
-  /usr/lib
-  /usr/local/lib64
-  /usr/local/lib
-  /sw/lib
-  /opt/local/lib
-  DOC "The EGL library")
-
-IF(EGL_INCLUDE_PATH)
-  INCLUDE_DIRECTORIES(${EGL_INCLUDE_PATH})
-  SET(EGL_FOUND 1 CACHE STRING "Set to 1 if EGL is found, 0 otherwise")
-ELSE(EGL_INCLUDE_PATH)
-  SET(EGL_FOUND 0 CACHE STRING "Set to 1 if EGL is found, 0 otherwise")
-ENDIF(EGL_INCLUDE_PATH)
 
 # Find mesa source code.
 FIND_PATH(MESA_SOURCE_PREFIX src/mesa/main/texobj.c
@@ -51,5 +24,3 @@ SET(MESA_SOURCE_FOUND 1 CACHE STRING "Set to 1 if mesa source code is found, 0 o
 ELSE(MESA_SOURCE_PREFIX)
 SET(MESA_SOURCE_FOUND 0 CACHE STRING "Set to 1 if mesa source code is found, 0 otherwise")
 ENDIF(MESA_SOURCE_PREFIX)
-
-MARK_AS_ADVANCED(EGL_FOUND)
