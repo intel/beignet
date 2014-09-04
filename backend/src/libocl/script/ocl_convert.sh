b94489f56ee3af3fc0eb32acade484db984120d3
Junyan He <junyan.he@linux.intel.com>
2014-09-04 22:53:00 +0800

    Add copyright header for all libocl files.
    Signed-off-by: Junyan He <junyan.he@linux.intel.com>
    Reviewed-by: Zhigang Gong <zhigang.gong@linux.intel.com>

--- backend/src/libocl/script/ocl_convert.sh
+++ backend/src/libocl/script/ocl_convert.sh
@@ -1,5 +1,25 @@
 #! /bin/sh -e
 
+echo '
+/*
+ * Copyright © 2012 - 2014 Intel Corporatio
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+'
+
 if [ $1"a" = "-pa" ]; then
     echo "#ifndef __OCL_CONVERT_H__"
     echo "#define __OCL_CONVERT_H__"
