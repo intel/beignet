270ac05a4744402fd6d5a4bb3c1504c481b0a8cf
Ruiling Song <ruiling.song@intel.com>
2014-10-11 14:09:37 +0800

    GBE: add legalize pass to handle wide integers
    This legalize pass will break wider integers like i128/i256/... into shorter ones.
    The problem is how to choose the shorter type? From my observation,
    wide integer type always comes from shorter ones through 'zext' on small type
    or 'bitcast' on vectors, so we simply choose the type where it comes from.
    Then we can split wide integer operations into operations on shorter interger.
    
    v2:
      add an assert on the wide integer bit-width, should be power of 2.
      use rpo_iterator to make sure traverse Value def before its use.
    
    v3:
      drop all references before erase processed instruction.
    
    Signed-off-by: Ruiling Song <ruiling.song@intel.com>
    Reviewed-by: Zhigang Gong <zhigang.gong@linux.intel.com>

--- backend/src/llvm/llvm_to_gen.cpp
+++ backend/src/llvm/llvm_to_gen.cpp
@@ -259,6 +259,7 @@ namespace gbe
       passes.add(createGVNPass());                  // Remove redundancies
     passes.add(createPrintfParserPass());
     passes.add(createScalarizePass());        // Expand all vector ops
+    passes.add(createLegalizePass());
     passes.add(createDeadInstEliminationPass());  // Remove simplified instructions
     passes.add(createCFGSimplificationPass());     // Merge & remove BBs
     passes.add(createScalarizePass());        // Expand all vector ops
