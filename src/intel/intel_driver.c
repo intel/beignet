0550850807160bcb7075c69d34177c324cd78343
Zhigang Gong <zhigang.gong@intel.com>
2015-04-14 09:51:46 +0800

    runtime: don't try to open nonexistent render nodes or device files.
    Signed-off-by: Zhigang Gong <zhigang.gong@intel.com>
    Reviewed-by: "Yang, Rong R" <rong.r.yang@intel.com>

--- src/intel/intel_driver.c
+++ src/intel/intel_driver.c
@@ -221,6 +221,8 @@ intel_driver_open(intel_driver_t *intel, cl_context_prop props)
     char card_name[20];
     for(cardi = 0; cardi < 16; cardi++) {
       sprintf(card_name, "/dev/dri/renderD%d", 128+cardi);
+      if (access(card_name, R_OK) != 0)
+        continue;
       if(intel_driver_init_render(intel, card_name))
         break;
     }
@@ -230,6 +232,8 @@ intel_driver_open(intel_driver_t *intel, cl_context_prop props)
     char card_name[20];
     for(cardi = 0; cardi < 16; cardi++) {
       sprintf(card_name, "/dev/dri/card%d", cardi);
+      if (access(card_name, R_OK) != 0)
+        continue;
       if(intel_driver_init_master(intel, card_name))
         break;
     }
@@ -323,7 +327,6 @@ intel_driver_init_render(intel_driver_t *driver, const char* dev_name)
 {
   int dev_fd, ret;
 
-  // usually dev_name = "/dev/dri/renderD%d"
   dev_fd = open(dev_name, O_RDWR);
   if (dev_fd == -1)
     return 0;
