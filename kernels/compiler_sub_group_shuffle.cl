1257a871669ea3e5ab9f3bdc885d69d41c6a522c
Guo Yejun <yejun.guo@intel.com>
2015-05-12 20:44:08 +0800

    add utest for intel_sub_group_shuffle
    v2: correct kernel to be suitable for simd_width both 8 and 16
    Signed-off-by: Guo Yejun <yejun.guo@intel.com>
    Reviewed-by: Zhigang Gong <zhigang.gong@intel.com>

---
+++ kernels/compiler_sub_group_shuffle.cl
@@ -0,0 +1,18 @@
+__kernel void compiler_sub_group_shuffle(global int *dst, int c)
+{
+  int i = get_global_id(0);
+  if (i == 0)
+    dst[0] = get_sub_group_size();
+  dst++;
+
+  int from = i;
+  int j = get_sub_group_size() - get_sub_group_id() - 1;
+  int o0 = get_sub_group_id();
+  int o1 = intel_sub_group_shuffle(from, c);
+  int o2 = intel_sub_group_shuffle(from, 5);
+  int o3 = intel_sub_group_shuffle(from, j);
+  dst[i*4] = o0;
+  dst[i*4+1] = o1;
+  dst[i*4+2] = o2;
+  dst[i*4+3] = o3;
+}
---
+++ utests/compiler_sub_group_shuffle.cpp
@@ -0,0 +1,45 @@
+#include "utest_helper.hpp"
+
+void compiler_sub_group_shuffle(void)
+{
+  const size_t n = 32;
+  const int32_t buf_size = 4 * n + 1;
+
+  // Setup kernel and buffers
+  OCL_CREATE_KERNEL("compiler_sub_group_shuffle");
+  OCL_CREATE_BUFFER(buf[0], 0, buf_size * sizeof(int), NULL);
+  OCL_SET_ARG(0, sizeof(cl_mem), &buf[0]);
+
+  int c = 3;
+  OCL_SET_ARG(1, sizeof(int), &c);
+
+  globals[0] = n;
+  locals[0] = 16;
+
+  OCL_MAP_BUFFER(0);
+  for (int32_t i = 0; i < buf_size; ++i)
+    ((int*)buf_data[0])[i] = -1;
+  OCL_UNMAP_BUFFER(0);
+
+  // Run the kernel on GPU
+  OCL_NDRANGE(1);
+
+  // Compare
+  OCL_MAP_BUFFER(0);
+  int* dst = (int *)buf_data[0];
+  int suggroupsize = dst[0];
+  OCL_ASSERT(suggroupsize == 8 || suggroupsize == 16);
+
+  dst++;
+  for (int32_t i = 0; i < (int32_t) n; ++i){
+    int round = i / suggroupsize;
+    int index = i % suggroupsize;
+    OCL_ASSERT(index == dst[4*i]);
+    OCL_ASSERT((round * suggroupsize + c) == dst[4*i+1]);
+    OCL_ASSERT((round * suggroupsize + 5) == dst[4*i+2]);
+    OCL_ASSERT((round * suggroupsize + (suggroupsize - index - 1)) == dst[4*i+3]);
+  }
+  OCL_UNMAP_BUFFER(0);
+}
+
+MAKE_UTEST_FROM_FUNCTION(compiler_sub_group_shuffle);
