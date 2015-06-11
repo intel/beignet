d95f9fb729246e50a850dca09f9d4b45dacc5179
Junyan He <junyan.he@linux.intel.com>
2015-06-11 19:24:59 +0800

    libocl: Add the builtin xxx_as functions for half.
    Signed-off-by: Junyan He <junyan.he@linux.intel.com>
    Reviewed-by: "Yang, Rong R" <rong.r.yang@intel.com>

--- backend/src/libocl/script/ocl_as.sh
+++ backend/src/libocl/script/ocl_as.sh
@@ -31,7 +31,7 @@ else
 fi
 
 # Supported base types and their lengths
-TYPES="long:8 ulong:8 int:4 uint:4 short:2 ushort:2 char:1 uchar:1 double:8 float:4"
+TYPES="long:8 ulong:8 int:4 uint:4 short:2 ushort:2 char:1 uchar:1 double:8 float:4 half:2"
 # Supported vector lengths
 VECTOR_LENGTHS="1 2 3 4 8 16"
 ROUNDING_MODES="rte rtz rtp rtn"
