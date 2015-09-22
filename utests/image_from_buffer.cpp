afd9b061c268b4237b9c15f7e18e47871668e6bb
Luo Xionghu <xionghu.luo@intel.com>
2015-09-22 14:44:53 +0800

    add utest for creating 2d image from buffer.
     v2: check cl_khr_image2d_from_buffer support first;
     use CL_DEVICE_IMAGE_BASE_ADDRESS_ALIGNMENT to allocate memory.
     v3: fix clGetDeviceInfo use.
    
    Signed-off-by: Luo Xionghu <xionghu.luo@intel.com>
    Reviewed-by: Guo, Yejun <yejun.guo@intel.com>

---
+++ utests/image_from_buffer.cpp
@@ -0,0 +1,82 @@
+#include <string.h>
+#include "utest_helper.hpp"
+#include <malloc.h>
+#include <cstring>
+
+static void image_from_buffer(void)
+{
+  size_t param_value_size;
+  std::string extensionStr;
+  OCL_CALL (clGetPlatformInfo, platform, CL_PLATFORM_EXTENSIONS, 0, 0, &param_value_size);
+  std::vector<char> param_value(param_value_size);
+  OCL_CALL (clGetPlatformInfo, platform, CL_PLATFORM_EXTENSIONS, param_value_size, param_value.empty() ? NULL : &param_value.front(), &param_value_size);
+  if (!param_value.empty())
+    extensionStr = std::string(&param_value.front(), param_value_size-1);
+
+  if (!std::strstr(extensionStr.c_str(), "cl_khr_image2d_from_buffer")) {
+    return;
+  }
+
+  size_t base_address_alignment = 0;
+  OCL_CALL (clGetDeviceInfo, device, CL_DEVICE_IMAGE_BASE_ADDRESS_ALIGNMENT, sizeof(base_address_alignment), &base_address_alignment, NULL);
+  const size_t w = 512;
+  const size_t h = 512;
+  cl_image_format format;
+  cl_image_desc desc;
+  int error;
+
+  memset(&desc, 0x0, sizeof(cl_image_desc));
+  memset(&format, 0x0, sizeof(cl_image_format));
+
+  // Setup kernel and images
+  size_t buffer_sz = sizeof(uint32_t) * w * h;
+  //buf_data[0] = (uint32_t*) malloc(buffer_sz);
+  buf_data[0] = (uint32_t*)memalign(base_address_alignment, buffer_sz);
+  for (uint32_t j = 0; j < h; ++j)
+    for (uint32_t i = 0; i < w; i++)
+      ((uint32_t*)buf_data[0])[j * w + i] = j * w + i;
+
+  cl_mem buff = clCreateBuffer(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, buffer_sz, buf_data[0], &error);
+
+  OCL_ASSERT(error == CL_SUCCESS);
+  format.image_channel_order = CL_RGBA;
+  format.image_channel_data_type = CL_UNSIGNED_INT8;
+  desc.image_type = CL_MEM_OBJECT_IMAGE2D;
+  desc.image_width = w;
+  desc.image_height = h;
+  desc.image_row_pitch = w * sizeof(uint32_t);
+
+  desc.buffer = 0;
+  OCL_CREATE_IMAGE(buf[0], CL_MEM_COPY_HOST_PTR, &format, &desc, buf_data[0]);
+
+  desc.buffer = buff;
+  OCL_CREATE_IMAGE(buf[1], 0, &format, &desc, NULL);
+
+  free(buf_data[0]);
+  buf_data[0] = NULL;
+
+  // Check result
+  OCL_MAP_BUFFER_GTT(0);
+  OCL_MAP_BUFFER_GTT(1);
+  for (uint32_t j = 0; j < h; ++j)
+    for (uint32_t i = 0; i < w; i++)
+    {
+      //printf("%d,%d\n", ((uint32_t*)buf_data[0])[j * w + i], ((uint32_t*)buf_data[1])[j * w + i]);
+      OCL_ASSERT(((uint32_t*)buf_data[0])[j * w + i] == ((uint32_t*)buf_data[1])[j * w + i]);
+    }
+  OCL_UNMAP_BUFFER_GTT(0);
+  OCL_UNMAP_BUFFER_GTT(1);
+
+  //spec didn't tell the sequence of release buffer of image. so release either buffer or image first is ok here.
+  //we follow the rule of destroy the bo at the last release, then the access of buffer after release image is legal
+  //and vice verse.
+#if 1
+  clReleaseMemObject(buf[1]);
+  clReleaseMemObject(buff);
+#else
+  clReleaseMemObject(buff);
+  clReleaseMemObject(buf[1]);
+#endif
+}
+
+MAKE_UTEST_FROM_FUNCTION(image_from_buffer);
