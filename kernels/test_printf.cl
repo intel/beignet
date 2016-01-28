2d79819d40490c840d8b078100ae98510e3bc4f9
Junyan He <junyan.he@linux.intel.com>
2016-01-28 12:20:06 +0800

    Add several printf utest cases.
    Signed-off-by: Junyan He <junyan.he@linux.intel.com>
    Reviewed-by: Yan Wang <yan.wang@linux.intel.com>
    Reviewed-by: Junyan He <junyan.he@linux.intel.com>

--- kernels/test_printf.cl
+++ kernels/test_printf.cl
@@ -44,3 +44,49 @@ test_printf(void)
     printf("--- End to the printf test ---\n");
   }
 }
+
+__kernel void
+test_printf_1(void)
+{
+   printf("");// just test null printf
+}
+
+__kernel void
+test_printf_2(void)
+{
+   printf("float %f\n", 2.0);// just test a uniform const
+   printf("long %lx\n", 0xABCD1234CCCCDDDD);
+}
+
+__kernel void
+test_printf_3(char arg)
+{
+   printf("@@ arg from func arg is %c\n", arg);
+}
+
+__kernel void
+test_printf_4(void)
+{
+    int a = get_global_size(0);
+    int b = get_local_size(0);
+    int c = a + 1;
+    int d = b + 2;
+    int e = b * 2;
+    int f = c + 1;
+    int g = d + 2;
+    int h = e * 2;
+    int i = a + 1;
+    int j = c / 2;
+    int k = a * 2;
+    int l = c + 1;
+    int m = f + 2;
+    int n = g * 2;
+    int o = e * 2;
+    int p = a + 1;
+    int q = c / 2;
+    int r = a * 2;
+    int s = c + 1;
+    int t = f + 2;
+    printf("@@ Long result is %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+	   a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t);
+}
--- utests/test_printf.cpp
+++ utests/test_printf.cpp
@@ -16,3 +16,57 @@ void test_printf(void)
 }
 
 MAKE_UTEST_FROM_FUNCTION(test_printf);
+
+void test_printf_1(void)
+{
+  // Setup kernel and buffers
+  OCL_CREATE_KERNEL_FROM_FILE("test_printf", "test_printf_1");
+  globals[0] = 1;
+  locals[0] = 1;
+
+  // Run the kernel on GPU
+  OCL_NDRANGE(1);
+}
+
+MAKE_UTEST_FROM_FUNCTION(test_printf_1);
+
+void test_printf_2(void)
+{
+  // Setup kernel and buffers
+  OCL_CREATE_KERNEL_FROM_FILE("test_printf", "test_printf_2");
+  globals[0] = 4;
+  locals[0] = 2;
+
+  // Run the kernel on GPU
+  OCL_NDRANGE(1);
+}
+
+MAKE_UTEST_FROM_FUNCTION(test_printf_2);
+
+void test_printf_3(void)
+{
+  char c = '@';
+  // Setup kernel and buffers
+  OCL_CREATE_KERNEL_FROM_FILE("test_printf", "test_printf_3");
+  globals[0] = 1;
+  locals[0] = 1;
+  OCL_SET_ARG(0, sizeof(char), &c);
+
+  // Run the kernel on GPU
+  OCL_NDRANGE(1);
+}
+
+MAKE_UTEST_FROM_FUNCTION(test_printf_3);
+
+void test_printf_4(void)
+{
+  // Setup kernel and buffers
+  OCL_CREATE_KERNEL_FROM_FILE("test_printf", "test_printf_4");
+  globals[0] = 1;
+  locals[0] = 1;
+
+  // Run the kernel on GPU
+  OCL_NDRANGE(1);
+}
+
+MAKE_UTEST_FROM_FUNCTION(test_printf_4);
