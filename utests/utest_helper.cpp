9bc95cb554dde76b4446e1d5d04a081dcc6f0b52
Rebecca N. Palmer <rebecca_palmer@zoho.com>
2016-10-12 23:38:03 +0100

    Utests: Allow testing cl_intel_accelerator via ICD
    v3: Use extension check, not beignet check.  Treat claiming
    to have the extension but not having the kernel as a failure.
    v4: Make extension check a function, use stderr.
    
    Signed-off-by: Rebecca N. Palmer <rebecca_palmer@zoho.com>
    Reviewed-by: Chuanbo Weng <chuanbo.weng@intel.com>

--- utests/utest_helper.cpp
+++ utests/utest_helper.cpp
@@ -875,6 +875,24 @@ int cl_check_beignet(void)
   return 1;
 }
 
+int cl_check_motion_estimation(void)
+{
+  std::string extStr;
+  size_t param_value_size;
+  OCL_CALL(clGetDeviceInfo, device, CL_DEVICE_EXTENSIONS, 0, 0, &param_value_size);
+  std::vector<char> param_value(param_value_size);
+  OCL_CALL(clGetDeviceInfo, device, CL_DEVICE_EXTENSIONS, param_value_size,
+           param_value.empty() ? NULL : &param_value.front(), &param_value_size);
+  if (!param_value.empty())
+    extStr = std::string(&param_value.front(), param_value_size-1);
+
+  if (std::strstr(extStr.c_str(), "cl_intel_motion_estimation") == NULL) {
+    printf("No cl_intel_motion_estimation, Skip!");
+    return 0;
+  }
+  return 1;
+}
+
 int cl_check_subgroups(void)
 {
   std::string extStr;
