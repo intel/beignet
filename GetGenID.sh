634b0d14bde9fb3392feade5bd7d5776620e4f0a
Yang, Rong R <rong.r.yang@intel.com>
2016-11-28 19:32:45 +0800

    CMake: add an option to enable OpenCL 2.0.
    The OpenCL 2.0 is disable default, to enable it, using option
    -DENABLE_OPENCL_20 to enable it.
    Now skylake and newer devices support OpenCL 2.0, if enable OpenCL 2.0,
    LLVM 3.9 and libdrm 2.4.66 is required.
    
    Signed-off-by: Yang Rong <rong.r.yang@intel.com>
    Reviewed-by: Pan Xiuli <xiuli.pan@intel.com>

--- GetGenID.sh
+++ GetGenID.sh
@@ -12,34 +12,50 @@ genpciid+=(0d02 0d12 0d22 0d0a 0d1a 0d2a 0d06 0d16 0d26 0d0b 0d1b 0d2b 0d0e 0d1e
 genpciid+=(1602 1606 160a 160d 160e 1612 1616 161a 161d 161e 1622 1626 162a 162d 162e)
 #BSW
 genpciid+=(22b0 22b1 22b2 22b3)
+#Only enable OpenCL 2.0 after SKL.
 #SKL
-genpciid+=(1906 1916 1926 190e 191e 1902 1912 1932 190b 191b 192b 193b 190a 191a 192a 193a)
+genpciid_20=(1906 1916 1926 190e 191e 1902 1912 1932 190b 191b 192b 193b 190a 191a 192a 193a)
 #BXT
-genpciid+=(5a84 5a85)
+genpciid_20+=(5a84 5a85 1a84 1a85)
 #KBL
-genpciid+=(5906 5916 5926 5913 5921 5923 5927 5902 5912 5917)
-genpciid+=(590b 591b 593b 5908 590e 591e 5915 590a 591a 591d)
+genpciid_20+=(5906 5916 5926 5913 5921 5923 5927 5902 5912 5917)
+genpciid_20+=(590b 591b 593b 5908 590e 591e 5915 590a 591a 591d)
 pciid=($(lspci -nn | grep "\[8086:.*\]" -o | awk -F : '{print $2}' | awk -F ] '{print $1}'))
 n=${#pciid[*]}
 i=0
 m=${#genpciid[*]}
+t=${#genpciid_20[*]}
 j=0
 while [ $i -lt $n ]
 do
-    id1=${pciid[$i]}
-    let j=0
+  id1=${pciid[$i]}
+  let j=0
 
-    while [ $j -lt $m ]
-    do
-	id2=${genpciid[$j]}
+  while [ $j -lt $m ]
+  do
+    id2=${genpciid[$j]}
 
-	if [ ${id1} == ${id2} ]
-	then
-	    echo ${id1}
-	    exit 0
-	fi
-	let j=j+1
-    done
+    if [ ${id1} == ${id2} ]
+    then
+      echo ${id1}
+      exit 0
+    fi
+    let j=j+1
+  done
 
-    let i=i+1
+  let j=0
+  while [ $j -lt $t ]
+  do
+    id2=${genpciid_20[$j]}
+
+    if [ ${id1} == ${id2} ]
+    then
+      echo ${id1}
+      exit 1
+    fi
+    let j=j+1
+  done
+
+  let i=i+1
 done
+exit -1
