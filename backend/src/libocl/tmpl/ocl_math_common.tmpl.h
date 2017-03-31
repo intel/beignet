4d7fe190274962e1a6e17a23a8c010bba55ff08d
rander <rander.wang@intel.com>
2017-03-31 10:23:17 +0800

    backend: add double version of erfc
    cp from fdlibm and refine it
    
    Signed-off-by: rander <rander.wang@intel.com>
    Tested-by: Yang Rong <rong.r.yang@intel.com>

--- backend/src/libocl/tmpl/ocl_math_common.tmpl.h
+++ backend/src/libocl/tmpl/ocl_math_common.tmpl.h
@@ -34,6 +34,7 @@ OVERLOADABLE double atanh(double x);
 OVERLOADABLE double exp(double x);
 OVERLOADABLE double expm1(double x);
 OVERLOADABLE double erf(double x);
+OVERLOADABLE double erfc(double x);
 OVERLOADABLE double cbrt(double x);
 OVERLOADABLE double ceil(double x);
 OVERLOADABLE double copysign(double x, double y);
