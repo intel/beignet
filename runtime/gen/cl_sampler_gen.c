346fbbb9369592caa073b41049135ddb4321b117
Junyan He <junyan.he@intel.com>
2017-06-11 13:50:35 +0800

    Add cl_sampler_gen to implement cl_sampler for GEN device.
    Signed-off-by: Junyan He <junyan.he@intel.com>

---
+++ runtime/gen/cl_sampler_gen.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2012 Intel Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "cl_gen.h"
+
+LOCAL cl_int
+cl_sampler_create_gen(cl_device_id device, cl_sampler sampler)
+{
+  return CL_SUCCESS;
+}
+
+LOCAL void
+cl_sampler_delete_gen(cl_device_id device, cl_sampler sampler)
+{
+}
