84ab1c460858229ba4213e9004d173df719d7885
Yan Wang <yan.wang@linux.intel.com>
2017-06-12 17:07:32 +0800

    Add utest case for filling image by small region.
    It is used to reproduce the bug of allocations of conformance test.
    
    Signed-off-by: Yan Wang <yan.wang@linux.intel.com>
    Reviewed-by: Yang Rong <rong.r.yang@intel.com>

--- utests/compiler_fill_large_image.cpp
+++ utests/compiler_fill_large_image.cpp
@@ -164,3 +164,53 @@ static void compiler_fill_large_image_2(void)
 }
 
 MAKE_UTEST_FROM_FUNCTION(compiler_fill_large_image_2);
+
+static void compiler_fill_large_image_3(void)
+{
+  const size_t w = 8192;
+  const size_t h = 8192;
+  const size_t num_of_lines = 8;
+  size_t origin[3] = {0, 0, 0};
+  const size_t region[3] = {w, num_of_lines, 1};
+  cl_image_format format;
+  cl_image_desc desc;
+
+  memset(&desc, 0x0, sizeof(cl_image_desc));
+  memset(&format, 0x0, sizeof(cl_image_format));
+
+  // Setup kernel and images
+  buf_data[0] = (uint32_t*) malloc(sizeof(uint32_t) * w * num_of_lines * 4);
+  buf_data[1] = (uint32_t*) malloc(sizeof(uint32_t) * w * h * 4);
+
+  memset(buf_data[0], 0, sizeof(uint32_t) * w * num_of_lines * 4);
+  memset(buf_data[1], 0, sizeof(uint32_t) * w * h * 4);
+
+  for (uint32_t j = 0; j < num_of_lines; ++j)
+    for (uint32_t i = 0; i < w; i++)
+      for (uint32_t k = 0; k < 4; k++)
+        ((uint32_t*)buf_data[0])[(j * w + i) * 4 + k] = (uint32_t)rand();
+
+  format.image_channel_order = CL_RGBA;
+  format.image_channel_data_type = CL_UNSIGNED_INT32;
+  desc.image_type = CL_MEM_OBJECT_IMAGE2D;
+  desc.image_width = w;
+  desc.image_height = h;
+  desc.image_row_pitch = 0;
+  OCL_CREATE_IMAGE(buf[0], 0, &format, &desc, NULL);
+  OCL_WRITE_IMAGE(buf[0], origin, region, buf_data[0]);
+  OCL_READ_IMAGE(buf[0], origin, region, buf_data[1]);
+
+  // Check result
+  for (uint32_t j = 0; j < num_of_lines; ++j)
+    for (uint32_t i = 0; i < w; i++)
+      for (uint32_t k = 0; k < 4; k++)
+        OCL_ASSERT(((uint32_t*)buf_data[0])[(j * w + i) * 4 + k] ==
+          ((uint32_t*)buf_data[1])[(j * w + i) * 4 + k]);
+
+  free(buf_data[0]);
+  free(buf_data[1]);
+  buf_data[0] = NULL;
+  buf_data[1] = NULL;
+}
+
+MAKE_UTEST_FROM_FUNCTION(compiler_fill_large_image_3);
